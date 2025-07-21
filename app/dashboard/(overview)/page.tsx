9d6cff39fc15bde04818062703dcbf918e8e74f3
FrancoLembo <franco.lembo@grupotuportal.com>
2025-07-21 13:08:01 -0300

    Chapter 10, 11, 12, 13, 14

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,7 +7,6 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 
 export default async function Page() 
 {
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -15,7 +14,6 @@ export default async function Page()
       </h1>
       
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
