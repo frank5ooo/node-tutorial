934468108dc2198b20ae4513de3d9f3f578bcd0c
frank5ooo <francolembo04@gmail.com>
2025-07-23 01:16:07 -0300

    Chapter 15, 16

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,9 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import postgres from 'postgres';
 import { redirect } from 'next/navigation';
-
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+ 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export type State = {
@@ -114,9 +116,33 @@ export async function updateInvoice(
 
 export async function deleteInvoice(id: string) 
 {
-  throw new Error('Failed to Delete Invoice');
+  // throw new Error('Failed to Delete Invoice');
  
   // Unreachable code block
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ){
+    try 
+    {
+      await signIn('credentials', formData);
+    } 
+    catch (error) 
+    {
+      if (error instanceof AuthError) 
+      {
+        switch (error.type) 
+        {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,7 +9,7 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL1!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
@@ -32,7 +32,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -56,7 +55,7 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+    const invoiceCountPromise  = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
