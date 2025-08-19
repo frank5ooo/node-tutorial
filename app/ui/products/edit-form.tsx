33e144578def2d484bb10e177a87d07bee6fd351
frank5ooo <francolembo04@gmail.com>
2025-08-19 20:57:47 -0300

    Agrego sefa-action a updateProduct

--- app/ui/products/edit-form.tsx
+++ app/ui/products/edit-form.tsx
@@ -4,6 +4,7 @@ import { ProductsField } from "@/app/lib/definitions";
 import {
   CheckIcon,
   ClockIcon,
+  CurrencyDollarIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
@@ -20,16 +21,21 @@ export default function EditInvoiceForm({
   const [formData, setFormData] = useState({
     id: products[0].id,
     name: products[0].name,
-    price: products[0].price,
+    price: products[0].price/100,
   });
 
   console.log(formData);
-
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    const dataToSend = {
+      id: formData.id,
+      name: formData.name,
+      price: Math.round(Number(formData.price) * 100), // convertir a centavos solo al enviar
+    };
+
     try {
-      const result = await updateProduct(formData);
+      const result = await updateProduct(dataToSend);
 
       if (result?.serverError) {
         console.error(result?.serverError);
@@ -50,14 +56,58 @@ export default function EditInvoiceForm({
             Choose product
           </label>
           <div className="relative">
-            
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            <input
+              id="name"
+              name="name"
+              type="text"
+              step="0.01"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              aria-describedby="product-error"
+              value={formData.name}
+              onChange={(e) =>
+                setFormData((prev) => ({
+                  ...prev,
+                  name: e.target.value,
+                }))
+              }
+              required
+            />
+            <label
+              htmlFor="pending"
+              className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
+            ></label>
+          </div>
+          <label htmlFor="price" className="mb-2 block text-sm font-medium">
+            Choose an amount
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              <input
+                id="price"
+                name="price"
+                type="text"
+                step="0.01"
+                placeholder="Enter USD price"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="price-error"
+                value={formData.price}
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    price: Number(e.target.value),
+                  }))
+                }
+                required
+              />
+              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
           </div>
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
-          href="/dashboard/invoices"
+          href="/dashboard/products"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancel
